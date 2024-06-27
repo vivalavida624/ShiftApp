0fcb07b5bdf40ad4180fddf004b7872e62e0cde9
alfredmaomao <alfredmaomao@hotmail.com>
2024-06-26 20:46:47 -0400

    splash screen: First commit

--- app/src/main/java/com/map08/shiftapp/MainActivity.kt
+++ app/src/main/java/com/map08/shiftapp/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.map08.shiftapp.ui.theme.ShiftAppTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +23,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShiftAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
+fun MainScreen() {
+    var showSplash by remember { mutableStateOf(true) }
+
+    LaunchedEffect(key1 = true) {
+        delay(3000)
+        showSplash = false
+    }
+
+    if (showSplash) {
+        SplashScreen()
+    } else {
+        HomeScreen()
+    }
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome to ShiftApp",
+            fontSize = 30.sp
+        )
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-Column {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-    Text(
-        text = "Peng",
-        modifier = modifier
-    )
-}
 }
 
 @Preview(showBackground = true)
