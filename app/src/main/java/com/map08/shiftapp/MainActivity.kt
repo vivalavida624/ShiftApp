1397c0be47d0b9854e3c50d109cb27feeb1e9e71
alfredmaomao <alfredmaomao@hotmail.com>
2024-06-27 10:15:45 -0400

    SplashScreen log added

--- app/src/main/java/com/map08/shiftapp/MainActivity.kt
+++ app/src/main/java/com/map08/shiftapp/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.map08.shiftapp.ui.theme.ShiftAppTheme
 import kotlinx.coroutines.delay
@@ -51,10 +55,10 @@ fun SplashScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Welcome to ShiftApp",
-            fontSize = 30.sp
-        )
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(200.dp))
     }
 }
 
