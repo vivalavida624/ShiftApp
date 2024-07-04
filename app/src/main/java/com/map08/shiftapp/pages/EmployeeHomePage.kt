816fb4d3e9156005f4492ce37bab84aa7bc84b29
vivalavida624 <vivalavida624@gmail.com>
2024-07-04 06:26:01 -0400

    add log out for employee Home

--- app/src/main/java/com/map08/shiftapp/pages/EmployeeHomePage.kt
+++ app/src/main/java/com/map08/shiftapp/pages/EmployeeHomePage.kt
@@ -6,12 +6,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -23,7 +25,9 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.map08.shiftapp.AuthViewModel
 import com.map08.shiftapp.EmployeeNavItem
+import com.map08.shiftapp.ui.theme.ShiftAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmployeeHomePage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
 
@@ -37,6 +41,26 @@ fun EmployeeHomePage(modifier: Modifier = Modifier, navController: NavController
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Employee Home", fontSize = 20.sp)
+                },
+                actions = {
+                    TextButton(
+                        onClick = {
+                            authViewModel.signout()
+                            navController.navigate("login") {
+                                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                            }
+                        }
+                    ) {
+                        Text(text = "Log Out")
+                    }
+                }
+            )
+        },
         bottomBar = {
             NavigationBar {
                 employeeNavItemList.forEachIndexed { index, employeeNavItem ->
